d6d1bf7f34c028428e61d3473612220cac078863
Ankur Shukla <ankurshukla19961@gmail.com>
2025-09-02 15:56:41 +0530

    resume updated

--- ui/resume.py
+++ ui/resume.py
@@ -9,6 +9,6 @@ def render_resume(base_dir: Path):
         side_page(base_dir)
     with col3:
         st.markdown("## Resume\n")
-        resume_url = "https://drive.google.com/file/d/1ZHL_eMg-m4d8EH4rKVMSXNAwmENtFkfi/view"
+        resume_url = "https://drive.google.com/file/d/1a313QsCO9tekx-oiPl6gh8B5W4r5jpb_/view?usp=drivesdk"
         st.link_button("Open Resume in New Tab", resume_url, use_container_width=False)
         st.caption("If the link doesn't open, copy & paste: " + resume_url)
